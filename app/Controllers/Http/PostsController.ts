c200403cc761a4999dbae648f2e76eb985ed198c
Kyrllan Nogueira de Souza <kyrllan@iMac-de-Kyrllan.local>
2024-01-17 13:49:41 -0300

    created auth

--- app/Controllers/Http/PostsController.ts
+++ app/Controllers/Http/PostsController.ts
@@ -1,7 +1,46 @@
-// import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Post from 'App/Models/Post'
+//import Database from '@ioc:Adonis/Lucid/Database'
 
 export default class PostsController {
-  public index() {
-    return ['post 1', 'post 2', 'post 3']
+  public async index({}: HttpContextContract) {
+    const posts = await Post.query().orderBy('id', 'asc')
+
+    return posts
+  }
+
+  public async store({ request }: HttpContextContract) {
+    const data = request.only(['title', 'content'])
+    const post = await Post.create(data)
+
+    return post
+  }
+
+  public async show({ params }: HttpContextContract) {
+    //const post = await Database.rawQuery(`SELECT * FROM posts WHERE id = ${params.id}`)
+    const post = await Post.findOrFail(params.id)
+
+    /*     if (!post) {
+      return response.notFound({ message: 'Post not found' })
+    } */
+
+    return post
+  }
+
+  public async update({ params, request }: HttpContextContract) {
+    const post = await Post.findOrFail(params.id)
+    const data = request.only(['title', 'content'])
+
+    post.merge(data)
+
+    await post.save()
+
+    return post
+  }
+
+  public async destroy({ params }: HttpContextContract) {
+    const post = await Post.findOrFail(params.id)
+
+    await post.delete()
   }
 }
--- start/routes.ts
+++ start/routes.ts
@@ -21,3 +21,4 @@
 import Route from '@ioc:Adonis/Core/Route'
 
 Route.get('/', 'PostsController.index')
+Route.resource('/posts', 'PostsController').apiOnly()
